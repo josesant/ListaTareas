5629e8f431c76f9c886b0603f15515dcbd53e6bc
Jose Santillan <jose.m.sant.25@gmail.com>
2023-06-08 20:07:44 -0300

    ajuste de nueva altura

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import ToDoApp from './components/ToDoApp/ToDoApp';
 const App = () => {
     return (
         <div className="container-fluid p-0">
-            <div className=" d-flex vh-100 vw-100 justify-content-center " >
-                <div className="col-12 col-md-8 col-lg-5 d-flex align-items-center p-1" >
+            <div className=" d-flex vh-100 justify-content-center " >
+                <div className="col-12 col-md-8 col-lg-5 d-flex align-items-start p-1 mt-3" >
                     <ToDoApp />
                 </div>
             </div>
